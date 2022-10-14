f0a81c4211c1ae7677941959eea8aea9e853ace4
Vinícius Neto <viniciusbsneto@gmail.com>
2022-10-14 02:30:55 -0300

    feat: add Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react'
+import { clsx } from 'clsx'
+import { Slot } from '@radix-ui/react-slot'
+
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg'
+  children: ReactNode
+  asChild?: boolean
+}
+
+export function Text({ size = 'md', children, asChild }: TextProps) {
+  const Component = asChild ? Slot : 'span'
+
+  return (
+    <Component
+      className={
+        clsx('text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg'
+        }
+      )}
+    >
+      {children}
+    </Component>
+  )
+}
