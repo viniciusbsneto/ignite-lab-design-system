be9507ab9be87a32c252d10b30a644bdfac591ba
Vinícius Neto <viniciusbsneto@gmail.com>
2022-10-14 02:37:54 -0300

    feat: add Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react'
+import { clsx } from 'clsx'
+import { Slot } from '@radix-ui/react-slot'
+
+
+export interface HeadingProps {
+  size?: 'sm' | 'md' | 'lg'
+  children: ReactNode
+  asChild?: boolean
+}
+
+export function Heading({ size = 'md', children, asChild }: HeadingProps) {
+  const Component = asChild ? Slot : 'h2'
+
+  return (
+    <Component
+      className={
+        clsx('text-gray-100 font-bold font-sans',
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg'
+        }
+      )}
+    >
+      {children}
+    </Component>
+  )
+}
