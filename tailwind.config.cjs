b40025a69c9a350500657b66b4cfe040b68a3e2e
Vinícius Neto <viniciusbsneto@gmail.com>
2022-10-14 03:51:02 -0300

    refactor: change tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,17 +11,23 @@ module.exports = {
       '2xl': 32
     },
     colors: {
+      transparent: 'transparent',
+
       black: '#000000',
       white: '#FFFFFF',
 
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6'
+      },
 
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#9BE1FB'
+      cyan: {
+        500: '#81D8F7',
+        300: '#9BE1FB'
+      }
     },
     extend: {
       fontFamily: {
