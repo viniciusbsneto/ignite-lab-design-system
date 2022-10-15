1db9fedce46baf43598bf3d3a7072a0a8b7fcc99
Vinícius Neto <viniciusbsneto@gmail.com>
2022-10-15 13:15:12 -0300

    feat: add className prop to Heading component

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,9 +7,10 @@ export interface HeadingProps {
   size?: 'sm' | 'md' | 'lg'
   children: ReactNode
   asChild?: boolean
+  className?: string
 }
 
-export function Heading({ size = 'md', children, asChild }: HeadingProps) {
+export function Heading({ size = 'md', children, asChild, className }: HeadingProps) {
   const Component = asChild ? Slot : 'h2'
 
   return (
@@ -20,7 +21,8 @@ export function Heading({ size = 'md', children, asChild }: HeadingProps) {
           'text-lg': size === 'sm',
           'text-xl': size === 'md',
           'text-2xl': size === 'lg'
-        }
+        },
+        className
       )}
     >
       {children}
