eb724733e12ecc64384ffa8f579d6af57b981194
Vinícius Neto <viniciusbsneto@gmail.com>
2022-10-15 13:28:35 -0300

    feat: add SignIn page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { SignIn } from './pages/SignIn'
+
 import './styles/global.css'
 
 export function App() {
-  return (<>
-    <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
-    <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">Enviar</button>
-  </>
+  return (
+    <SignIn />
   )
 }
---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SignIn } from './SignIn'
+
+export default {
+  title: 'Pages/Sign In',
+  component: SignIn,
+  args: {},
+  argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {}
